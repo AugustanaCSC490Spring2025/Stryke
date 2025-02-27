cf8887cb7bc8639193fbd320609b5abc4116e2be
Johnny Breeden <johnnybreeden21@augustana.edu>
2025-02-27 14:40:41 -0600

    started creating the intro-screen

---
+++ stryke_main/lib/intro_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class IntroScreen extends StatelessWidget {
+  const IntroScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Padding(
+          padding: EdgeInsets.all(50),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
