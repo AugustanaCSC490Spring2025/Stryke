1db53e92ba1f80f6ff0973b5abd77a0b38207a22
Johnny Breeden <johnnybreeden21@augustana.edu>
2025-03-13 14:40:47 -0500

    refactored

--- stryke_main/lib/screens/intro/views/intro_screen.dart
+++ stryke_main/lib/screens/intro/views/intro_screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import '../../../utils/button_styles.dart';
 import '../../../utils/spacing.dart';
 import '../../../utils/text_styles.dart';
-import '../../intro/views/sign_in_screen.dart';
+import '../../intro/views/sign_up_screen.dart';
 import 'login_screen.dart';
 
 class IntroScreen extends StatefulWidget {
@@ -62,7 +62,7 @@ class _IntroScreenState extends State<IntroScreen> with TickerProviderStateMixin
                         context,
                         MaterialPageRoute<void>(
                             builder: (context) =>
-                            const SignInScreen()),
+                            const SignUnScreen()),
                       );
                     },
                     style: ButtonStyles.colorButton(
