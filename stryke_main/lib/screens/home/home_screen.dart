b5f06a724080fb3a137f0fc8b9d345c42302abf8
Tommy Anderson <tommyanderson21@augustana.edu>
2025-03-18 13:52:29 -0500

    Testing firebase pull

--- stryke_main/lib/screens/home/home_screen.dart
+++ stryke_main/lib/screens/home/home_screen.dart
@@ -10,11 +10,9 @@ import '../intro/views/splash_screen.dart';
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
-
   @override
   State<HomePage> createState() => _HomePageState();
 }
-
 
 class _HomePageState extends State<HomePage> {
   final _authService = Authentication();
